58bf9358e280510caea10141971c491a91b77a37
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-08-27 21:04:58 -0700

    feat: Update home page view to include "Add your first todo!" message

--- lists/tests.py
+++ lists/tests.py
@@ -4,5 +4,6 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html_2(self):
         response = self.client.get("/")
         self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertContains(response, "<p>Add your first todo!</p>")
         self.assertContains(response, "<html>")
         self.assertContains(response, "</html>")
--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return HttpResponse("<html><title>To-Do lists</title></html>")
+    return HttpResponse("<html><title>To-Do lists</title><p>Add your first todo!</p></html>")
